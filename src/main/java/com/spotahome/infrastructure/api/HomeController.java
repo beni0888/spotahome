70ab4f838149461a88b47c830d71b7f058c8ed47
Jesús Miguel Benito Calzada <jesus.benito@stubhub.com>
2019-03-19 11:09:11 +0100

    Add download homes API

--- src/main/java/com/spotahome/infrastructure/api/HomeController.java
+++ src/main/java/com/spotahome/infrastructure/api/HomeController.java
@@ -1,25 +1,66 @@
 package com.spotahome.infrastructure.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spotahome.application.GetHomes;
 import com.spotahome.application.GetHomesQuery;
 import com.spotahome.domain.Home;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/homes")
+@RequestMapping("/api")
 public class HomeController {
+
+    private static final String DOWNLOADED_FILE_NAME = "homes.json";
+    private static final String DEFAULT_SORTING_FIELD = "id";
+
+
     @Autowired
     private GetHomes getHomes;
 
-    @GetMapping
-    public List<Home> listHomes(@RequestParam("sortedBy") String sortedBy) {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping(path = "/homes")
+    public List<Home> listHomes(@RequestParam(value = "sortedBy", defaultValue = DEFAULT_SORTING_FIELD) String sortedBy) {
         GetHomesQuery query = new GetHomesQuery(sortedBy);
         return getHomes.execute(query);
     }
+
+    @GetMapping(path = "/download")
+    public ResponseEntity<Resource> downloadHomes(
+            @RequestParam(value = "sortedBy", defaultValue = DEFAULT_SORTING_FIELD) String sortedBy)
+            throws JsonProcessingException {
+
+        List<Home> homes = getHomes.execute(new GetHomesQuery(sortedBy));
+        ByteArrayResource jsonResource = new ByteArrayResource(objectMapper.writeValueAsString(homes).getBytes(StandardCharsets.UTF_8));
+
+        return ResponseEntity.ok()
+                .headers(getHttpHeaders())
+                .contentLength(jsonResource.contentLength())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(jsonResource);
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+        headers.add(HttpHeaders.PRAGMA, "no-cache");
+        headers.add(HttpHeaders.EXPIRES, "0");
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=%s", DOWNLOADED_FILE_NAME));
+        return headers;
+    }
+
 }
--- src/test/java/com/spotahome/infrastructure/api/HomeControllerTest.java
+++ src/test/java/com/spotahome/infrastructure/api/HomeControllerTest.java
@@ -1,5 +1,6 @@
 package com.spotahome.infrastructure.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spotahome.application.GetHomes;
 import com.spotahome.application.GetHomesQuery;
 import com.spotahome.domain.Home;
@@ -10,14 +11,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.net.URI;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,6 +35,9 @@ public class HomeControllerTest {
 
     @MockBean
     private GetHomes getHomes;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     public void whenListHomesApiIsCalled_thenItReturnsAListOfHomes() throws Exception {
@@ -58,6 +65,23 @@ public class HomeControllerTest {
 
                 .andExpect(status().isOk());
 
+    }
+
+    @Test
+    public void whenDownloadApiIsCalled_thenItDownloadsAJsonFile() throws Exception {
+        List<Home> homes = asList(
+                aHome(1L, "home 1", "Madrid", "http://foo.com/home1", "http://foo.com/home1"),
+                aHome(2L, "home 2", "Madrid", "http://foo.com/home2", "http://foo.com/home2")
+        );
+        GetHomesQuery query = new GetHomesQuery("title");
+        when(getHomes.execute(query)).thenReturn(homes);
+
+        MvcResult result = mvc.perform(get("/api/download?sortedBy=title"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(result.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(homes));
+        assertThat(result.getResponse().getContentType()).isEqualTo(APPLICATION_OCTET_STREAM.toString());
     }
 
     private Home aHome(Long id, String title, String city, String url, String picture) {
